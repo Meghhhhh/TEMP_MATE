13a719c92e7408ca55a4f348e93acd359f696fd8
Priyal208 <priyalrwl.20@gmail.com>
2025-02-22 09:04:16 +0530

    Config fpr deploy frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import { config } from "dotenv";
+import react from "@vitejs/plugin-react";
+// Load environment variables from .env file
+config();
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  server: {
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 3000
+    host: "0.0.0.0", // Allow external access
   },
-})
+  define: {
+    "process.env": process.env,
+  },
+});

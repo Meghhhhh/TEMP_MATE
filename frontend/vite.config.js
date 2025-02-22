395f481a286bd09e949f9ddf4d1f4164decdc9f1
Priyal208 <priyalrwl.20@gmail.com>
2025-02-22 09:10:43 +0530

    Config frotned config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,11 @@ config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: process.env.PORT || 5173, // Use the PORT environment variable or default to 3000
     host: "0.0.0.0", // Allow external access
